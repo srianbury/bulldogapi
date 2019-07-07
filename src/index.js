7c2b7036fc3c006fbf126e1a86b62079dc52a4ef
Brian Sunbury <bsunbury29@gmail.com>
2019-07-07 14:57:54 -0700

    starting express
    added, express and cors, wrote the first route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('hello from express');
+});
+
+app.listen(3000, ()=>
+    console.log('listening on port 3000')
+);
 
 console.log('hello world');
 console.log(process.env.TEST_KEY);
