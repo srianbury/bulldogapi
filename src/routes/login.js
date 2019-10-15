448a8d4d23b430bd8570669b4e5df69c0558c187
Brian Sunbury <bsunbury29@gmail.com>
2019-10-14 21:32:18 -0700

    match username and password

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,13 +4,29 @@ import jwt from 'jsonwebtoken';
 const router = Router();
 
 router.post('/', async (req, res) => {
-    const { username } = req.body;
+    const user = await verifyLogin(req);
 
-    jwt.sign({ username }, 'scrtky', (err, token) => {
-        res.json({
-            token
+    if(user){
+        jwt.sign({ user }, 'scrtky', (err, token) => {
+            if(!err){
+                res.json({ user, token });
+            } else {
+                res.status(500).json({ err });
+            }
         });
-    });
+    } else {
+        res.status(401).json({
+            error: 'Username and password do not match.'
+        });
+    }
 });
+
+async function verifyLogin(req){
+    const { username: givenUname, password: givenPwd } = req.body;
+    const user = await req.context.models.User.findByLogin(givenUname);
+    const { username: dbUname, password: dbPwd } = await user;
+
+    return givenPwd===dbPwd ? user : null;
+}
 
 export default router;
