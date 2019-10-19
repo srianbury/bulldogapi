c3a3ed905b6290cce8fa86877bee3fa14a1384b4
Brian Sunbury <bsunbury29@gmail.com>
2019-10-19 15:21:43 -0700

    example of param middleware

--- src/routes/message.js
+++ src/routes/message.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { verifyToken } from '../funcs';
+import { check } from 'express-validator';
+import { useParameterValidation } from '../middleware';
 
 const router = Router();
 
@@ -8,14 +10,23 @@ router.get('/', async (req, res) => {
     return res.send(messages);
 });
 
-router.post('/', verifyToken, async (req, res) => {
-    const message = await req.context.models.Message.create({   
-        text: req.body.text,
-        user: req.userInfo.user._id
-    });
-
-    res.json({ message });
-});
+const requiredPostParams = [
+    check('text').isString(),
+];
+router.post(
+    '/', 
+    verifyToken, 
+    requiredPostParams, 
+    useParameterValidation,
+    async (req, res) => {
+        const message = await req.context.models.Message.create({   
+            text: req.body.text,
+            user: req.userInfo.user._id
+        });
+    
+        res.json({ message });
+    }
+);
 
 router.get('/:messageId', async (req, res) => {
     const message = await req.context.models.Message.findById(req.params.messageId);
