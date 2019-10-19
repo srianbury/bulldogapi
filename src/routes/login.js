e93f83d77e7301a7cca8f0dee4ffd9166a418b67
Brian Sunbury <bsunbury29@gmail.com>
2019-10-19 15:21:18 -0700

    handle if user does not exist in login

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,55 +1,45 @@
-import { Router } from 'express';
-import jwt from 'jsonwebtoken';
-import { decrypt } from '../funcs';
-const { check, validationResult } = require('express-validator');
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import { decrypt } from "../funcs";
 
 const router = Router();
 
-const loginParams = [
-    check('username').isString(),
-    check('password').isLength({ min: 6 })
-];
-router.post('/', loginParams, async (req, res) => {
-    const missingParams = validationResult(req);
-    if(missingParams.isEmpty()){
-        const user = await getUser(req);
-        if(user){
-            const ok = await verifyLogin(user, req);
-            if(ok){
-                jwt.sign({ user }, process.env.JWT_SCRT_KEY, (err, token) => {
-                    if(!err){
-                        res.json({ user, token });
-                    } else {
-                        res.status(500).json({ err });
-                    }
-                });
-            } else {
-                res.status(401).json({
-                    error: 'Username and password do not match.'
-                });
-            }
+router.post("/", async (req, res) => {
+  const user = await getUser(req);
+  if (user) {
+    const ok = await verifyLogin(user, req);
+    if (ok) {
+      jwt.sign({ user }, process.env.JWT_SCRT_KEY, (err, token) => {
+        if (!err) {
+          res.json({ user, token });
         } else {
-            res.status(404).json({ error: 'Username does not exist.' });
+          res.status(500).json({ err });
         }
+      });
     } else {
-        res.status(422).json({ error: missingParams.array() });
+      res.status(401).json({
+        error: "Username and password do not match."
+      });
     }
+  } else {
+    res.status(404).json({ error: "Username does not exist." });
+  }
 });
 
-async function getUser(req){
-    const { username: givenUname, password: givenPwd } = req.body;
-    const user = await req.context.models.User.findByLogin(givenUname);
-    return user;
+async function getUser(req) {
+  const { username: givenUname, password: givenPwd } = req.body;
+  const user = await req.context.models.User.findByLogin(givenUname);
+  return user;
 }
 
-async function verifyLogin(user, req){
-    const { password: givenPwd } = req.body;    
-    const { _id: uid } = user;
+async function verifyLogin(user, req) {
+  const { password: givenPwd } = req.body;
+  const { _id: uid } = user;
 
-    const userPwd = await req.context.models.UserPassword.findByUid(uid);
-    const { password: dbPwd } = await userPwd;
+  const userPwd = await req.context.models.UserPassword.findByUid(uid);
+  const { password: dbPwd } = await userPwd;
 
-    return givenPwd===decrypt(dbPwd) ? user : null;
+  return givenPwd === decrypt(dbPwd) ? user : null;
 }
 
 export default router;
