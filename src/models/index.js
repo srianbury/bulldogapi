893ff48f54d22a167077233f52f083f792ae967f
Brian Sunbury <bsunbury29@gmail.com>
2019-11-16 09:00:24 -0700

    added blurp and gallery to models

--- src/models/index.js
+++ src/models/index.js
@@ -1,20 +1,23 @@
-import mongoose from 'mongoose';
-import User from './user';
-import UserPassword from './password';
-import Dog from './dogs';
-import Litter from './litters';
+import mongoose from "mongoose";
+import User from "./user";
+import UserPassword from "./password";
+import Dog from "./dogs";
+import Litter from "./litters";
+import Blurp from "./blurps";
+import Gallery from "./gallery";
 
-function connectDb(){
-    return mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+function connectDb() {
+  return mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 }
 
 const models = {
-    User,
-    UserPassword,
-    Dog,
-    Litter
+  User,
+  UserPassword,
+  Dog,
+  Litter,
+  Blurp,
+  Gallery
 };
-
 
 export default models;
 export { connectDb };
