e52d598544af3419083217de7337bf213a0e61e3
Brian Sunbury <bsunbury29@gmail.com>
2019-11-23 09:45:17 -0700

    update parents

--- src/routes/dog.js
+++ src/routes/dog.js
@@ -1,22 +1,25 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-    const dogs = await req.context.models.Dog.find();
-    return res.json({ data: dogs });
+router.get("/", async (req, res) => {
+  const dogs = await req.context.models.Dog.find();
+  return res.json({ data: dogs });
 });
 
-
-router.get('/:id', async (req, res) => {
-    const dog = await req.context.models.Dog.findById(req.params.id);
-    return res.json({ data: dog });
+router.get("/:id", async (req, res) => {
+  const dog = await req.context.models.Dog.findById(req.params.id);
+  return res.json({ data: dog });
 });
 
-
-router.put('/:id', async (req, res) => {
-    const data = req.body;
-    return res.json({ data });  
+router.put("/:id", async (req, res) => {
+  const data = req.body;
+  const updatedDocument = await req.context.models.Dog.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: data },
+    { new: true }
+  );
+  return res.json({ data: updatedDocument });
 });
 
 export default router;
