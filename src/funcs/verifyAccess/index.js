07fabeff92de0bf5158ab2e585a3f3c0833c9314
Brian Sunbury <bsunbury29@gmail.com>
2019-12-05 00:03:50 -0700

    verify user has access to route

--- src/funcs/verifyAccess/index.js
+++ src/funcs/verifyAccess/index.js
@@ -3,15 +3,26 @@ import { ACCESS } from "../../constants";
 /*
     verify that the user has access to the given resource
 */
-async function verifyAccessInList(allowedRoles, req) {
-  const { _id } = req.userInfo.user;
-  const user = await req.models.User.findById(_id);
-  const accessLevel = user.access;
-  return allowedRoles.includes(accessLevel);
+async function verifyAccessInList(allowedRoles, req, res, next) {
+  try {
+    const { _id } = req.userInfo.user;
+    const user = await req.context.models.User.findById(_id);
+    const accessLevel = user.access;
+    if (allowedRoles.includes(accessLevel)) {
+      next();
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    console.log(e);
+    const error = "Insufficient permission.";
+    return res.status(401).json({ error });
+  }
 }
 
-function verifyAdminAccess(req, res, next) {
-  next();
+async function verifyAdminAccess(req, res, next) {
+  const allowedRoles = [ACCESS.ADMIN, ACCESS.MINDFLAYER];
+  return await verifyAccessInList(allowedRoles, req, res, next);
 }
 
-export default { verifyAdminAccess };
+export { verifyAdminAccess };
