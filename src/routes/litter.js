1e9129640c19d8c8bd7846bd4d105efd915a23f8
Brian Sunbury <bsunbury29@gmail.com>
2019-12-08 22:27:34 -0700

    put router to update litter

--- src/routes/litter.js
+++ src/routes/litter.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import { verifyToken, verifyAdminAccess } from "../funcs";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
   const litters = await req.context.models.Litter.find();
   return res.json({ data: litters });
+});
+
+router.put("/:id", verifyToken, verifyAdminAccess, async (req, res) => {
+  const data = req.body;
+  const updatedDocument = await req.context.models.Litter.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: data },
+    { new: true }
+  );
+  return res.json({ data: updatedDocument });
 });
 
 export default router;
